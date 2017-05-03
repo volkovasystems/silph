5aba9fdcd7aa4afdf41bfa85572979a46990598d
johnlenon <johnlenonmaghanoy@gmail.com>
2017-05-03 18:18:05 +0800

    Updated test.

--- silph-test.js
+++ silph-test.js
@@ -1,3 +1,4 @@
+const assert = require( "assert" );
 const silph = require( "./silph.js" );
 
 var item_object = {
@@ -23,42 +24,15 @@ var merchant_array = [
 	}
 ];
 
-var result1 = silph( item_object, "item.title" );
+assert.equal( silph( item_object, "item.title" ), "Burger Item" );
+assert.equal( silph( item_object, "item.name" ), "burger-item" );
+assert.deepEqual( silph( item_object, "item" ), { "name": "burger-item", "title": "Burger Item" } );
+assert.equal( silph( item_array, "0" ), "ballpen-item" );
+assert.equal( silph( item_array, "1" ), "Ballpen Item" );
+assert.equal( silph( merchant_array, "0" ), "biyaheroes-team" );
+assert.equal( silph( merchant_array, "1" ), "pure-gold" );
+assert.deepEqual( silph( merchant_array, "2" ), { "logo": { "reference": "ref1", "path": "path1" } } );
+assert.deepEqual( silph( merchant_array, "2.logo" ), { "reference": "ref1", "path": "path1" } );
+assert.equal( silph( merchant_array, "2.logo.reference" ), "ref1" );
 
-var result2 = silph( item_object, "item.name" );
-
-var result3 = silph( item_object, "item" );
-
-var result4 = silph( item_array, "0" );
-
-var result5 = silph( item_array, "1" );
-
-var result6 = silph( merchant_array, "0" );
-
-var result7 = silph( merchant_array, "1" );
-
-var result8 = silph( merchant_array, "2" );
-
-var result9 = silph( merchant_array, "2.logo" );
-
-var result10 = silph( merchant_array, "2.logo.reference" );
-
-console.log( "result 1", result1 );
-
-console.log( "result 2", result2 );
-
-console.log( "result 3", result3 )
-
-console.log( "result 4", result4 );
-
-console.log( "result5", result5 );
-
-console.log( "result 6", result6 );
-
-console.log( "result 7", result7 );
-
-console.log( "result 8", result8 );
-
-console.log( "result 9", result9 );
-
-console.log( "result 10", result10 );
+console.log( "ok" );
